cd9573a1f60c39454b7c45c15fc6ef78c9fd02be
wangxianlin <756316064@qq.com>
2020-02-28 00:08:54 +0800

    引入首页

---
+++ src/main/java/com/bs/flower/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.bs.flower.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String toIndex(){
+        return "index";
+    }
+}
